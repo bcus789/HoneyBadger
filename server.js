ad6c3ab8fd68a79be111a5069aff2b1737039ad3
Charlie NoFace <youremail@gmail.comh.e.ergaster@gmail.comgit config --global user.email youremail@gmail.com>
2019-10-18 17:59:24 -0700

    second try

--- server.js
+++ server.js
@@ -64,13 +64,6 @@ app.get('/scrape', function(req, res) {
     });
   });
 });
-var MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
-
-mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI, {
-  useMongoClient: true,
-});
 
 // Start the server
 app.listen(PORT, function() {
